68f33d81ff55a8d7284d5d7aaea47824c630a570
Cory Green <1elitehacker@gmail.com>
2016-05-12 09:44:45 -0400

    Create index.php
    Adding CSS

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<HTML>
+<HEAD>
+<title> Decitonne </title>
+</HEAD>
+<BODY>
+<br><br><br>
+<img src="logo.png"></img>
+<form>
+  <input type="text" name="query"><br>
+</form
+</BODY>
+</HTML>
