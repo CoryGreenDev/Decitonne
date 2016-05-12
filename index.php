0b573e0e5fdfacc1c1a62111158f189c55cb1146
Cory Green <1elitehacker@gmail.com>
2016-05-12 09:52:51 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,11 +3,13 @@
 <HEAD>
 <title> Decitonne </title>
 </HEAD>
+<center>
 <BODY>
 <br><br><br>
 <img src="logo.png"></img>
 <form>
   <input type="text" name="query"><br>
 </form
+</center>
 </BODY>
 </HTML>
